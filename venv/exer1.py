488c9e6bf51b226280f7f11c3f2aaf710c4ed59a
lex <ade77amos@gmail.com>
2024-12-20 09:41:44 -0600

    created the Web app for todos

--- venv/exer1.py
+++ venv/exer1.py
@@ -1,25 +1,32 @@
 import exerc1_func as Ex
 import FreeSimpleGUI as sg
-
+sg.theme("Black")
 label1 = sg.Text("Enter feet")
 label2 = sg.Text("Enter Inches")
 input_box1 = sg.Input()
 input_box2 = sg.Input()
+Exit_1 = sg.Button("Exit")
 convert = sg.Button('Convert')
 output_label = sg.Text("", key="output")
 Window = sg.Window("Converter",
                   layout=[[label1,input_box1],
                           [label2,input_box2],
-                          [convert,output_label ]])
+                          [convert,output_label ],[Exit_1]])
 while True:
     event, values = Window.read()
     print(event)
     print(values)
-
-    feet = float(values[1])
-    inche = float(values[0])
-       
-    new_convert=Ex.conv(feet , inche)
-    Window['output'].update(value=f"{new_convert} m", text_color="green")
+    match event:
+        case "Convert":
+            try:
+                feet = float(values[1])
+                inche = float(values[0])
+            
+                new_convert=Ex.conv(feet , inche)
+                Window['output'].update(value=f"{new_convert} m", text_color="green")
+            except ValueError:
+                sg.popup("Please provide two numbers.")
+        case "Exit":
+            break
 Window.close()
 
---
+++ venv/web.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import function
+
+todos = function.get_todo()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    function.write_todo(todos)
+
+
+st.title("MY Todo APP")
+st.subheader(" This is my todo app")
+st.write("this app is to increase your productivity")
+
+
+
+
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todo(todos)
+        del st.session_state[todo]
+        st.rerun()
+st.text_input(label="", placeholder="Add new todo...", 
+              on_change=add_todo, key="new_todo")
+
+"""print("Hello")
+st.session_state"""
